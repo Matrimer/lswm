029e1ee2fe2547e14f4020ea15ecfa2d266dcb5f
Matrimer <44270049+Matrimer@users.noreply.github.com>
2019-07-30 00:43:28 +0200

    Added volume controll and larger border width.

--- config.def.h
+++ config.def.h
@@ -11,7 +11,7 @@
 
 /** generic settings **/
 #define MASTER_SIZE     0.52
-#define SHOW_PANEL      True      /* show panel by default on exec */
+#define SHOW_PANEL      False     /* show panel by default on exec */
 #define TOP_PANEL       True      /* False means panel is on bottom */
 #define PANEL_HEIGHT    18        /* 0 for no space for panel, thus no panel */
 #define DEFAULT_MODE    TILE      /* initial layout/mode: TILE MONOCLE BSTACK FLOAT */
@@ -21,10 +21,20 @@
 #define FOLLOW_MOUSE    False     /* focus the window the mouse just entered */
 #define CLICK_TO_FOCUS  True      /* focus an unfocused window when clicked  */
 #define FOCUS_BUTTON    Button3   /* mouse button to be used along with CLICK_TO_FOCUS */
-#define BORDER_WIDTH    3         /* window border width */
+#define BORDER_WIDTH    5         /* window border width */
 #define FOCUS           "#ff950e" /* focused window border color   */
 #define UNFOCUS         "#444444" /* unfocused window border color */
 #define INFOCUS         "#9c3885" /* focused window border color on unfocused monitor */
+
+/* dmenu-appearance */
+static const char dmenufont[]       = "monospace:size=10";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
+
+
 #define MINWSZ          50        /* minimum window size in pixels */
 #define DEFAULT_MONITOR 0         /* the monitor to focus initially */
 #define DEFAULT_DESKTOP 0         /* the desktop to focus initially */
@@ -75,8 +85,18 @@ static const AppRule rules[] = { \
  * custom commands
  * must always end with ', NULL };'
  */
-static const char *termcmd[] = { "xterm", NULL };
-static const char *dmenucmd[] = { "dmenu_run", NULL };
+/* commands */
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+
+static const char *termcmd[]  = { "st", NULL };
+
+static const char *surfcmd[]  = { "tabbed", "surf", "-e", NULL };
+static const char *torsurf[]  = { "torsocks", "tabbed", "surf", "-e", NULL };
+
+static const char *mpctogglecmd[] = { "mpc", "-p", "7600", "toggle", NULL };
+const char* voldown[]  = {"amixer","set","MPD","1%-",NULL};
+const char* volup[]    = {"amixer","set","MPD","1%+",NULL};
 
 
 #define MONITORCHANGE(K,N) \
@@ -95,36 +115,40 @@ static Key keys[] = {
     {  MOD1,             XK_b,          togglepanel,       {NULL}},
     {  MOD1,             XK_BackSpace,  focusurgent,       {NULL}},
     {  MOD1,             XK_q,          killclient,        {NULL}},
-    {  MOD1,             XK_n,          next_win,          {NULL}},
-    {  MOD1,             XK_o,          prev_win,          {NULL}},
-    {  MOD1,             XK_h,          resize_master,     {.i = -5}}, /* decrease size in px */
-    {  MOD1,             XK_l,          resize_master,     {.i = +5}}, /* increase size in px */
-    {  MOD1,             XK_o,          resize_stack,      {.i = -5}}, /* shrink   size in px */
-    {  MOD1,             XK_g,          resize_stack,      {.i = +5}}, /* grow     size in px */
+    {  MOD1,             XK_o,          next_win,          {NULL}},
+    {  MOD1,             XK_n,          prev_win,          {NULL}},
+    {  MOD1,             XK_h,          resize_master,     {.i = -10}}, /* decrease size in px */
+    {  MOD1,             XK_d,          resize_master,     {.i = +10}}, /* increase size in px */
+    {  MOD1|SHIFT|CONTROL, XK_o,          resize_stack,      {.i = -10}}, /* shrink   size in px */
+    {  MOD1|SHIFT|CONTROL, XK_n,          resize_stack,      {.i = +10}}, /* grow     size in px */
     {  MOD1|CONTROL,     XK_e,          rotate,            {.i = -1}},
     {  MOD1|CONTROL,     XK_i,          rotate,            {.i = +1}},
     {  MOD1|SHIFT,       XK_e,          rotate_filled,     {.i = -1}},
     {  MOD1|SHIFT,       XK_i,          rotate_filled,     {.i = +1}},
     {  MOD1,             XK_Tab,        last_desktop,      {NULL}},
     {  MOD1,             XK_Return,     swap_master,       {NULL}},
-    {  MOD1|SHIFT,       XK_n,          move_down,         {NULL}},
-    {  MOD1|SHIFT,       XK_o,          move_up,           {NULL}},
+    {  MOD1|SHIFT,       XK_o,          move_down,         {NULL}},
+    {  MOD1|SHIFT,       XK_n,          move_up,           {NULL}},
     {  MOD1|SHIFT,       XK_t,          switch_mode,       {.i = TILE}},
     {  MOD1|SHIFT,       XK_m,          switch_mode,       {.i = MONOCLE}},
     {  MOD1|SHIFT,       XK_b,          switch_mode,       {.i = BSTACK}},
     {  MOD1|SHIFT,       XK_f,          switch_mode,       {.i = FLOAT}},
     {  MOD1|CONTROL,     XK_q,          quit,              {.i = 0}}, /* quit with exit value 0 */
-    {  MOD1|CONTROL,     XK_r,          quit,              {.i = 1}}, /* quit with exit value 1 */
     {  MOD1,             XK_space,      spawn,             {.com = termcmd}},
     {  MOD1,             XK_p,          spawn,             {.com = dmenucmd}},
-    {  MOD4,             XK_n,          moveresize,        {.v = (int []){   0,  25,   0,   0 }}}, /* move down  */
-    {  MOD4,             XK_o,          moveresize,        {.v = (int []){   0, -25,   0,   0 }}}, /* move up    */
-    {  MOD4,             XK_i,          moveresize,        {.v = (int []){  25,   0,   0,   0 }}}, /* move right */
-    {  MOD4,             XK_e,          moveresize,        {.v = (int []){ -25,   0,   0,   0 }}}, /* move left  */
-    {  MOD4|SHIFT,       XK_n,          moveresize,        {.v = (int []){   0,   0,   0,  25 }}}, /* height grow   */
-    {  MOD4|SHIFT,       XK_o,          moveresize,        {.v = (int []){   0,   0,   0, -25 }}}, /* height shrink */
-    {  MOD4|SHIFT,       XK_i,          moveresize,        {.v = (int []){   0,   0,  25,   0 }}}, /* width grow    */
-    {  MOD4|SHIFT,       XK_e,          moveresize,        {.v = (int []){   0,   0, -25,   0 }}}, /* width shrink  */
+    {  MOD1,		     XK_Escape,	    spawn,		       {.com = mpctogglecmd}},
+    {  MOD1,		     XK_s,		    spawn,		       {.com = surfcmd}},
+    {  MOD4,		     XK_s,		    spawn,		       {.com = torsurf}},
+    {  MOD1,		     XK_w,		    spawn,		       {.com = volup}},
+    {  MOD1,		     XK_r,		    spawn,		       {.com = voldown}},
+    {  MOD4,             XK_e,          moveresize,        {.v = (int []){   0,  25,   0,   0 }}}, /* move down  */
+    {  MOD4,             XK_i,          moveresize,        {.v = (int []){   0, -25,   0,   0 }}}, /* move up    */
+    {  MOD4,             XK_o,          moveresize,        {.v = (int []){  25,   0,   0,   0 }}}, /* move right */
+    {  MOD4,             XK_n,          moveresize,        {.v = (int []){ -25,   0,   0,   0 }}}, /* move left  */
+    {  MOD4|SHIFT,       XK_e,          moveresize,        {.v = (int []){   0,   0,   0,  25 }}}, /* height grow   */
+    {  MOD4|SHIFT,       XK_i,          moveresize,        {.v = (int []){   0,   0,   0, -25 }}}, /* height shrink */
+    {  MOD4|SHIFT,       XK_o,          moveresize,        {.v = (int []){   0,   0,  25,   0 }}}, /* width grow    */
+    {  MOD4|SHIFT,       XK_n,          moveresize,        {.v = (int []){   0,   0, -25,   0 }}}, /* width shrink  */
        DESKTOPCHANGE(    XK_F1,                             0)
        DESKTOPCHANGE(    XK_F2,                             1)
        DESKTOPCHANGE(    XK_F3,                             2)
